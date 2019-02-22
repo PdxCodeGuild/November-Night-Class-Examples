58b01e0ea0d75525380a7bf33894132280cc17fe
Zack Kollar <rallokkcaz@gmail.com>
2019-02-21 19:20:39 -0800

    Added django-usersystem example

---
+++ django-usersystem/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+
+from accounts import views
+
+app_name = 'accounts'
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    path('register', views.register, name='register'),
+    path('profile', views.profile, name='profile'),
+]
---
+++ django-usersystem/accounts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.conf import settings
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
